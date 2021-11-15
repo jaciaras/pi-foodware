6fdc4351371e0efa05434f6ec5f9e0ecfd4be110
jaciaras <jaaci.rodrigues@gmail.com>
2021-11-15 20:23:47 -0300

    v1

---
+++ src/components/ItemsList.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="items-list">
+        <Loading v-if="isLoading" />
+        <Item v-for="item in itemsList" :key="item.id" :item="item" />
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Item from './Item';
+import Loading from './Loading';
+export default {
+    name: 'ItemsList',
+    components: {
+        Item,
+        Loading  
+    },
+    data() {
+        return {
+            itemsList: [],
+            isLoading: false
+        }
+    },
+    created() {},
+    computed: {
+        selectedCategory: {
+            get() {
+                return this.$store.state.selectedCategory;
+            }
+        }
+    },
+    methods: {
+        getItemsList() {
+            this.isLoading = true;
+            this.itemsList = [];
+            setTimeout( () => {
+                axios.get(`http://localhost:3000/${this.selectedCategory}`).then((response) => {
+                    this.itemsList = response.data;
+                    this.isLoading = false;
+                });
+            }, 2000);
+        }
+    },
+    watch: {
+        selectedCategory() {
+            this.getItemsList();
+        }
+}};
+</script>
+
+<style style="less" scoped>
+    .items-list {
+        width: 100%;
+        margin: 50px;
+        display: flex;
+        flex-wrap: wrap;
+
+        @media @tablets {
+        flex-wrap: wrap;
+        margin: 20px;
+        margin: 0px;
+        padding: 20px;
+    }
+    }
+</style>
